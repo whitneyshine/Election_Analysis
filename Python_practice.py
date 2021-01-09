5afcce5af5b18859040a1c3ec7b0113ceeafd8f7
whitneyshine <wshine79@sbcglobal.net>
2021-01-08 20:26:22 -0600

    Updated per new download

---
+++ Python_practice.py
@@ -0,0 +1,84 @@
+##counties=["Arapahoe","Denver","Jefferson"]
+    ##if counties[1]=='Denver":
+##print(counties[1])
+    ##if counties[3] ! = "Jefferson"
+##print(counties[2])
+
+# ##temperature = int(input("What is the temperature outside?"))
+
+# ##if temperature > 80:
+#     ##print("Turn on the AC")
+# ##else:
+#     ##print("Open the Windows")
+
+
+# #What is the score?
+# #score = int(input("What is the test score?"))
+
+# #Determine the grade.
+# # if score >=90:
+# #     print("Your grade is an A")
+# # else:
+# #     if score >=80:
+# #         print("Your grade is a B")
+# #     else:
+# #         if score >=70:
+# #             print("Your grade is a C")
+# #         else:
+# #             if score >=60:
+# #                 print("Your grade is a D")
+# #             else:
+# #                 print("Your grade is an F")
+
+# #What is the score?
+# score = int(input("What is your test score?"))
+
+# #Determine the grade.
+# if score >= 90:
+#     print("Your grade is an A")
+# elif score >= 80:
+#     print("Your grade is a B")
+# elif score >= 70:
+#     print("Your grade is a C")
+# elif score >= 60:
+#     print("Your grade is a D")
+# else:
+#     print("Your grade is an F")
+
+    
+##ounties = ["Arapahoe", "Denver", "Jefferson"]
+##if "Arapahoe" in counties and "El Paso" not in counties:
+    ##print("Only Arapahoe is in the list of counties")
+##else:
+    ##print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
+##x = 0
+##while x <= 5:
+    ##print(x)
+    ##x = x + 1
+##count = 7
+
+# ##while count < 1:
+#     ##print("Hello World")
+
+# ##counties = ["Arapahoe", "Denver", "Jefferson"]
+# ##for county in counties:
+#     ##print(county)
+# ##numbers = [0,1,2,3,4]
+# for num in numbers:
+#     ##print(num)
+# ##for num in range(0):
+#    ## print (num)
+# ##for i in range(len(counties)):
+#     #3print(counties[i])
+
+# counties = ["Arapahoe", "Denver", "Jefferson"]
+
+# voting_data = []
+# voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+# voting_data.append({"county":"Denver", "registered_voters": 463353})
+# voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+
+# voting_data
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}

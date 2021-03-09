6e7772de821699e24f370a3dff132e78810fc05c
whitneyshine <wshine79@sbcglobal.net>
2021-03-08 20:56:29 -0600

    Finishing up Deliverable 1.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -109,14 +109,14 @@ with open(file_to_save, "w") as txt_file:
         #6f:  Write an if statement to determine the winning county and get its vote count.
         if (county_vote_count > largest_county_voter_turnout):
             largest_county_voter_turnout = county_vote_count
-            largest_county = county_list
+            largest_county = county_name
     
     #7: Print the county with the largest to the terminal.
-    largest_county_turnout_results = (
-        f"\n-----------------------\n"
+    largest_county_voter_turnout = (
+        f"-----------------------\n"
         f"Largest County Turnout: {largest_county}\n"
-        f"-----------------------\n\n")
-    print(largest_county_turnout_results)
+        f"-----------------------\n")
+    print(largest_county_voter_turnout, end="")
     
     #8: Save the county with the largest turnout to a text file.
 
